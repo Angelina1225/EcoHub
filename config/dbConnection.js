acb7ef51f8afbfc65adfc872e6fa14b7ea8fde39
Dhanvin Patel <78669309+dhanvinpatel@users.noreply.github.com>
2024-11-03 13:46:34 -0500

    Added exit error code

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
 dotenv.config();
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URI;
 
 // Connect to MongoDB Atlas
 const connectDB = async () => {
@@ -14,7 +14,9 @@ const connectDB = async () => {
         console.log('====================================');
         console.log('Error connecting to MongoDB: ', error);
         console.log('====================================');
+
+        process.exit(1);
     }
-}
+};
 
 export default connectDB;
