2c76d97fb5a5d21537f6364bc70fcc6024936c44
Dhanvin Patel <78669309+dhanvinpatel@users.noreply.github.com>
2024-12-02 12:57:13 -0500

    Added form validation

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,12 @@
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import {GoogleUser} from '../models/User.js';
+import { GoogleUser } from '../models/User.js';
 
 async function configurePassport(passport) {
     passport.use(new GoogleStrategy({
-            clientID: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: '/auth/google/callback',
-        },
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback',
+    },
         async (accessToken, refreshToken, profile, cb) => {
             const newGoogleUser = {
                 googleId: profile.id,
@@ -14,17 +14,17 @@ async function configurePassport(passport) {
                 lastName: profile.name.familyName,
                 email: profile.emails[0].value,
                 image: profile.photos[0].value
-            }
+            };
 
-            try{
-                let user = await GoogleUser.findOne({googleId: profile.id})
-                if(user){
+            try {
+                let user = await GoogleUser.findOne({ googleId: profile.id });
+                if (user) {
                     cb(null, user)
-                } else{
-                    user = await GoogleUser.create(newGoogleUser)
-                    cb(null, user)
+                } else {
+                    user = await GoogleUser.create(newGoogleUser);
+                    cb(null, user);
                 }
-            }catch(e){
+            } catch (e) {
                 console.log(e);
             }
         }
@@ -35,7 +35,7 @@ async function configurePassport(passport) {
             cb(null, user.id);
         });
     });
-      
+
     passport.deserializeUser((user, cb) => {
         process.nextTick(() => {
             cb(null, user);
