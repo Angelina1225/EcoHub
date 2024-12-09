52f546720b9aff72e897ba45bcf1ebe99d211d22
Dhanvin Patel <78669309+dhanvinpatel@users.noreply.github.com>
2024-12-09 17:43:31 -0500

    Removed controllers directory

--- controllers/userController.js
+++
@@ -1,8 +0,0 @@
-import User from '../models/User.js';
-
-const getUsers = async (req, res) => {
-    const users = await User.find();
-    return res.render('home', { users });
-};
-
-export { getUsers };
